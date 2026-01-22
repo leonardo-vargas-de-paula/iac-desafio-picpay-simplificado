cec6fdfd8424eec6883308ac8726e0bc36620640
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-01-22 09:08:12 -0400

    feat: esconde variavel do ip

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -27,10 +27,10 @@ resource "aws_security_group" "back_picpay_sg" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   security_group_id = aws_security_group.back_picpay_sg.id
-  cidr_ipv4   = "<ip>/32"
-  from_port   = 22
-  ip_protocol = "tcp"
-  to_port     = 22
+  cidr_ipv4         = "${var.ip_ssh}/32"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_http" {
